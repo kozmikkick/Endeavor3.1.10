7a2bb704baa10b443a12a03377f51c41de126128
Timothy Nelson <nelson.ecp@gmail.com>
2013-02-26 20:29:54 -0800

    mach-tegra: tegra_pmqos: Setting boost freq to 1600

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -24,7 +24,7 @@
  */
 
 /* in kHz */
-#define BOOST_CPU_FREQ_MIN	1500000
+#define BOOST_CPU_FREQ_MIN	1600000
 #define CAP_CPU_FREQ_MAX	475000
 #define T3_CPU_MIN_FREQ         51000
 #define T3_LP_MAX_FREQ     	475000
