a36f82faf36a37e69451b3fa8750898fd37b6288
Timothy Nelson <nelson.ecp@gmail.com>
2013-02-28 12:07:28 -0800

    mach-tegra: tegra3_clocks: Increasing to quad 1600

--- arch/arm/mach-tegra/tegra3_clocks.c
+++ arch/arm/mach-tegra/tegra3_clocks.c
@@ -4873,7 +4873,7 @@ static struct tegra_cpufreq_table_data cpufreq_tables[] = {
 	{ freq_table_1p3GHz, 1, 10 },
 	{ freq_table_1p4GHz, 1, 11 },
 	{ freq_table_1p5GHz, 1, 13 },
-	{ freq_table_1p6GHz, 1, 13 },
+	{ freq_table_1p6GHz, 1, 14 },
 	{ freq_table_1p7GHz, 1, 12 },
 };
 
